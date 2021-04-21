fde5e47e5fd04def1822558ad0c654ad9598a096
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-04-21 16:33:21 -0500

    mystery function

---
+++ mysteryProgram.js
@@ -0,0 +1,17 @@
+function WhatDoesThisProgramDo(lst) {
+    let current = lst.head;
+    while (current !== null) {
+        let newNode = current;
+        while (newNode.next !== null) {
+            if (newNode.next.value === current.value) {
+                newNode.next = newNode.next.next;
+            }
+            else {
+                newNode = newNode.next;
+            }
+        }
+        current = current.next;
+    }
+}
+
+//O(n^2) quadratic, it traverses the list once for each node in the list This program removes duplicates from a linked list
